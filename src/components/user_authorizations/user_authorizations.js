77d63702d9f3dc893e9663479403e1e4183d7cee
KonstantinKonshu <kostyanfly@gmail.com>
2020-06-05 23:44:37 +0600

    minor fix style auth

--- src/components/user_authorizations/user_authorizations.js
+++ src/components/user_authorizations/user_authorizations.js
@@ -13,40 +13,44 @@ class UserAuthorizations extends Component {
   render() {
     console.log("this.state", this.state);
     return (
-      <div className="cp-ua-auth_form">
-        <div className="cp-ua-auth_form-header">Login</div>
+      <div className="cp-ua-auth_main_container">
+          <div className="cp-ua-auth_form">
+              <div className="cp-ua-auth_form-header">Authorization</div>
 
-        <input
-          name="login"
-          className="cp-ua-auth_form-input_email"
-          type="text"
-          placeholder="Login"
-          onChange={e => {
-            this.setState({
-              login_user: e.target.value
-            });
-          }}
-          value={this.state.login_user}
-        />
+              <input
+                  name="login"
+                  className="cp-ua-auth_form-input_email"
+                  type="text"
+                  placeholder="Login"
+                  onChange={e => {
+                      this.setState({
+                          login_user: e.target.value
+                      });
+                  }}
+                  value={this.state.login_user}
+              />
 
-        <input
-          className="cp-ua-auth_form-input_password"
-          type="password"
-          placeholder="Password"
-          onChange={e => {
-            this.setState({
-              password: e.target.value
-            });
-          }}
-          value={this.state.password}
-        />
+              <input
+                  className="cp-ua-auth_form-input_password"
+                  type="password"
+                  placeholder="Password"
+                  onChange={e => {
+                      this.setState({
+                          password: e.target.value
+                      });
+                  }}
+                  value={this.state.password}
+              />
 
-        <div
-          className="cp-ua-auth_form-btn_login"
-          onClick={() => {
-            console.log("Hello btn");
-          }}
-        >{`LOGIN`}</div>
+              <div
+                  className="cp-ua-auth_form-btn_login"
+                  onClick={() => {
+                      console.log("Hello btn");
+                  }}
+              >
+                  LOGIN
+              </div>
+          </div>
       </div>
     );
   }
